eaacc0cbe00a1ad629d07411bf1c769731b02e7a
Romain Pelisse <belaran@gmail.com>
2015-10-06 15:17:33 +0200

    lab1: code clean

--- lab1/src/main/java/org/jboss/as/labs/datagrid/lab1/HotrodClient.java
+++ lab1/src/main/java/org/jboss/as/labs/datagrid/lab1/HotrodClient.java
@@ -7,6 +7,9 @@ import org.infinispan.commons.api.BasicCacheContainer;
 
 public class HotrodClient {
 
+	private static final String SERVER_NAME = "localhost";
+	private static final int SERVER_PORT = 11222;
+	
 	public static void main(String[] args) {
 		ConfigurationBuilder cb	= new ConfigurationBuilder();
 		cb.tcpNoDelay(true).connectionPool()
@@ -15,16 +18,22 @@ public class HotrodClient {
 		      .testOnReturn(false)
 		      .testWhileIdle(true)
 		      .addServer()
-		      .host("localhost")
-		      .port(11222);
+		      .host(SERVER_NAME)
+		      .port(SERVER_PORT);
 		
         BasicCacheContainer cacheContainer = new RemoteCacheManager(cb.build());
         BasicCache<String, String> cache = cacheContainer.getCache(); //FIXME: set a cache name and configure it to have a eviction of 100000
 
+        final String key = "car";
+        String value = "ferrari";
         // FIXME: change the code below to import 100000 items, then requests all the available keys - how many are there in the cache ?
-        cache.put("car", "ferrari");
-        assert cache.get("car").equals("ferrari");
+        cache.put(key, value);
+        assert cache.get(key).equals(value);
+        System.out.println("Stored value for '" + key + "' is :" + cache.get(key));
+        
         cache.remove("car");
         assert !cache.containsKey("car") : "Value must have been removed!";
+        System.exit(0);
+        
 	}
 }
